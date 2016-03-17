db070d034987c2747faafbb1aa1388fc983bd855
dmac <dmac100@users.noreply.github.com>
2016-03-17 23:05:42 +0000

    Added run-length encoding to IntStack.

--- src/backend/util/IntStack.java
+++ src/backend/util/IntStack.java
@@ -1,7 +1,10 @@
 package backend.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntStack {
-	private static final int NEGATIVE_OFFSET = 10;
+	private static final int NEGATIVE_OFFSET = 50;
 	
 	private byte[] values = new byte[1024];
 	private int count = 0;
@@ -19,32 +22,70 @@ public class IntStack {
 	}
 	
 	public void push(int value) {
-		ensureCapacity(size + 5);
+		count++;
 		
+		ensureCapacity(size + 6);
+		
+		// Store value as an offset of the last value.
 		int storedValue = value - topValue + NEGATIVE_OFFSET;
 		topValue = value;
 		
-		values[size++] = (byte)(storedValue & 0x7F);
-		storedValue >>>= 7;
-		while(storedValue > 0) {
-			values[size++] = (byte)((storedValue & 0x7F) | 0x80);
-			storedValue >>>= 7;
+		// Apply run-length encoding. The 7th bit being set indicates the first 6 bits
+		// represent the number of times the value given in the first 6 bits of the last
+		// byte is repeated.
+		if(storedValue < 0x40 && size > 0) {
+			if((values[size - 1] & 0xC0) == 0) {
+				if((values[size - 1] & 0x3F) == storedValue) {
+					// Save first repeated value.
+					values[size++] = 0x40;
+					return;
+				}
+			} else if((values[size - 1] & 0x80) == 0 && size > 1) {
+				if((values[size - 2] & 0x3F) == storedValue) {
+					if(values[size - 1] < 0x7F) {
+						// Increment repeated value.
+						values[size - 1] += 1;
+						return;
+					}
+				}
+			}
 		}
 		
-		count++;
+		// Variable-length encoding. The 8th bit being set indicates that there is another
+		// byte for this value.
+		values[size++] = (byte)(storedValue & 0x3F);
+		storedValue >>>= 6;
+		while(storedValue > 0) {
+			values[size++] = (byte)((storedValue & 0x3F) | 0x80);
+			storedValue >>>= 6;
+		}
 	}
 	
 	public int pop() {
-		int storedValue = (values[--size] & 0x7F);
-		while((values[size] & 0x80) > 0) {
-			storedValue <<= 7;
-			storedValue += (values[--size] & 0x7F);
+		count--;
+		
+		int storedValue;
+		
+		if((values[size - 1] & 0x40) > 0) {
+			// Read run-length encoding.
+			storedValue = values[size - 2];
+			if((values[size - 1] & 0x3F) == 0) {
+				size--;
+			} else {
+				values[size - 1]--;
+			}
+		} else {
+			// Read variable length value.
+			storedValue = (values[--size] & 0x3F);
+			while((values[size] & 0x80) > 0) {
+				storedValue <<= 6;
+				storedValue += (values[--size] & 0x3F);
+			}
 		}
 		
+		// Apply offset.
 		int value = topValue;
 		topValue = topValue - storedValue + NEGATIVE_OFFSET;
-		
-		count--;
 		
 		return value;
 	}
@@ -63,6 +104,14 @@ public class IntStack {
 			System.arraycopy(values, 0, newValues, 0, values.length);
 			values = newValues;
 		}
+	}
+	
+	protected List<String> getBinaryValues() {
+		List<String> binaryValues = new ArrayList<>();
+		for(int i = 0; i < size; i++) {
+			binaryValues.add(Integer.toBinaryString(values[i] & 0xFF));
+		}
+		return binaryValues;
 	}
 	
 	public int getStoredSize() {
