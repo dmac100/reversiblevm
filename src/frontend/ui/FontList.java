ee8a88348d0bb609473ce265b82a2aa4e730a3e4
dmac <dmac100@users.noreply.github.com>
2016-03-23 21:17:01 +0000

    Added fallbacks if consolas is not found.

--- src/frontend/ui/FontList.java
+++ src/frontend/ui/FontList.java
@@ -1,10 +1,36 @@
 package frontend.ui;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.widgets.Display;
 
 public class FontList {
-	public static Font consolas10 = new Font(Display.getCurrent(), "Consolas", 10, SWT.NORMAL);
-	public static Font consolas9 = new Font(Display.getCurrent(), "Consolas", 9, SWT.NORMAL);
+	private static final String monospaceName = getMonospaceName();
+	
+	public static final Font consolas10 = new Font(Display.getCurrent(), monospaceName, 10, SWT.NORMAL);
+	public static final Font consolas9 = new Font(Display.getCurrent(), monospaceName, 9, SWT.NORMAL);
+	
+	/**
+	 * Returns the first found monospace font.
+	 */
+	private static String getMonospaceName() {
+		Set<String> fonts = new HashSet<>();
+		
+		for(FontData fontData:Display.getCurrent().getFontList(null, true)) {
+			fonts.add(fontData.getName());
+		}
+		
+		for(String name:Arrays.asList("Consolas", "Courier", "Courier New")) {
+			if(fonts.contains(name)) {
+				return name;
+			}
+		}
+		
+		return "Monospace";
+	}
 }
