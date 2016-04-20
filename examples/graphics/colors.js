aadb590512c4b17482b175107564dcb356a42df4
dmac <dmac100@users.noreply.github.com>
2016-04-20 17:40:00 +0100

    Fixed math in colors.js

--- examples/graphics/colors.js
+++ examples/graphics/colors.js
@@ -12,7 +12,7 @@ digits.forEach(x =>
 @for(i <- colors.keys())
 	@rect(
 		x: 50 + (i % 6) * 20 + (Math.floor(i / 36) % 2) * 20 * 6,
-		y: 50 + Math.floor(i / 6) * 20 - (Math.floor((i + 36) / 2 * 36) * 20 * 6),
+		y: 50 + Math.floor(i / 6) * 20 - Math.floor((i + 36) / (36 * 2)) * 20 * 6,
 		width: 20,
 		height: 20,
 		fill: colors[i],
